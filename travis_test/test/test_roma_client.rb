43b27172e702b0beee96ce0302c0b0062df71df8
byplayer <byplayer100@gmail.com>
2017-10-16 12:26:07 +0800

    add assertion for set

--- travis_test/test/test_roma_client.rb
+++ travis_test/test/test_roma_client.rb
@@ -6,7 +6,7 @@ require 'roma/client'
 class RomaClientTest < Test::Unit::TestCase
   def test_get_set
     rc = Roma::Client::RomaClient.new(%w(localhost_11211 localhost_11311))
-    rc.set('hoge', 'fuga')
+    assert_equal('STORED', rc.set('hoge', 'fuga'))
 
     assert_equal('fuga', rc.get('hoge'))
   end
